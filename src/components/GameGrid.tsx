77cea544e99f9f1a38f9c998187da22eb1c92f0d
S Bulman <sm.bulman@gmail.com>
2024-06-21 10:02:02 +0900

    Refactoring- fetch games with React Query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -24,7 +24,7 @@ const GameGrid = ({ gameQuery }: Props) => {
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
         </GameCardContainer>
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../Services/api-client";
+import { FetchResponse } from "../Services/api-client";
 
 export interface Genre {
     id: number;
@@ -7,7 +10,14 @@ export interface Genre {
 }
 
 
-const useGenres = () => ({data: genres, isLoading: false,})
+const useGenres = () => useQuery({
+    queryKey: ['genres'],
+    queryFn: () => 
+        apiClient
+            .get<FetchResponse<Genre>>('/genres').then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: genres.length, results: genres }
+})
     
 
 export default useGenres;
