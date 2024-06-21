48bac417d9cc5b88058a5e07edd3b05f2ec39517
S Bulman <sm.bulman@gmail.com>
2024-06-21 14:13:03 +0900

    Refactoring- Creating a reusable API client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { GameQuery } from '../App';
-import apiClient from '../Services/api-client';
-import { FetchResponse } from '../Services/api-client';
+import APIClient, { FetchResponse } from '../Services/api-client';
 import { Platform } from './usePlatforms';
 
+const apiClient = new APIClient<Game>('/games');
 
 export interface Game {
     id: number;
@@ -23,16 +23,17 @@ useQuery<FetchResponse<Game>, Error>({
   queryKey: ['games', gameQuery],
   queryFn: () =>
     apiClient
-    .get<FetchResponse<Game>>('/games', {
-      params: {
-        genres: gameQuery.genre?.id, 
-        parent_platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      },
+      .getAll({
+        params: {
+          genres: gameQuery.genre?.id, 
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText
+        },
+
+      })
     })
-    .then(res => res.data)
-})
+
 
 
 export default useGames;
