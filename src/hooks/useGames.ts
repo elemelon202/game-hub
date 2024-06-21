72fe15ac99e089a02ed797797ef22aa239f9732d
S Bulman <sm.bulman@gmail.com>
2024-06-22 08:37:41 +0900

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -33,7 +33,8 @@ const useGames = (
       }),
       getNextPageParam: (lastPage, allPages) => {
         return lastPage.next ? allPages.length + 1 : undefined;
-      }
+      },
+      staleTime: 24 * 60 * 60 * 1000 //24h
     })
 
 
